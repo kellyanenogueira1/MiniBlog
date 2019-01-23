ec7fe9c1c14819e158d34e415150368a706293dc
= <=>
2019-01-23 14:34:32 -0200

    Bootstrap configurado!

--- index.php
+++ index.php
@@ -1,21 +1,30 @@
-<!doctype html>
+<!DOCTYPE html>
 
-<html>
-    <head lang="pt-br"> 
+<html lang="pt-br">
+    <head> 
         <title>MiniBlog - ProAluno</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <!-- <link rel="stylesheet" type="text/css" href="css/styles.css"> -->
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="bootstrap4/css/bootstrap.min.css">
 
     </head>
 
     <body>
+        <div>
+            <h1 class="font-weight-bold text-left">Blog ProALuno</h1>
+            <div>
+                <ul class="nav">
+                    <li></li>
+                </ul>
+            </div>
+
+        </div>
 
         <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js">/script>
+        <script src="bootstrap4/js/bootstrap.min.js"></script>
     </body>
 </html>
